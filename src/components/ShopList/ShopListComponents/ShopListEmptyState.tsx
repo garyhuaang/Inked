d1bbb29bf32c4bee6e8c4e71ce2caa1f5537c8fe
Gary Huang <garyhuaang@gmail.com>
2026-08-15 21:03:32 -0500

    INK-10: more refactoring for ShopList

--- src/components/ShopList/ShopListComponents/ShopListEmptyState.tsx
+++ src/components/ShopList/ShopListComponents/ShopListEmptyState.tsx
@@ -1,8 +1,6 @@
 import type { ShopListEmptyStateProps } from '../ShopList.types';
 import { ShopListSkeletons } from './ShopListSkeletons';
 
-/** What the list shows when there are no cards: placeholders while a search
- * is in flight, otherwise the no-results message. */
 export function ShopListEmptyState({ loading }: ShopListEmptyStateProps) {
   if (loading) return <ShopListSkeletons />;
 
